af9249c8f51669b60649dae5048d58507ce6f787
Servo MacPro <tarzanboycraft@hotmail.com>
2020-05-26 04:32:56 -0500

    imprimir rectngulo

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ for(let i=0;i<10;i++) {
     console.log(linea);
     linea = "";
 }
+
+console.log('\n')
+
+for(let i=0;i<10;i++) {
+    for (let j = 0; j < 10; j++) {
+        linea += "*";
+    }
+    console.log(linea);
+    linea = "";
+}
