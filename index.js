7dfce9c4109b42e5736322cc04643cc73352b476
compu escritorio <pepewicho_98@hotmail.com>
2020-05-26 03:58:59 -0500

    +triangulo

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log("Hola Mundo");
 console.log("Adios alv");
+
+let linea = "";
+
+for(let i=0;i<10;i++) {
+    for (let j = 0; j < i; j++) {
+        linea += "*";
+    }
+    console.log(linea);
+    linea = "";
+}
